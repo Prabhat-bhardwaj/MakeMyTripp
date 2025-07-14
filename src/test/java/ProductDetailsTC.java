ec3058e0d19c9445bf8d85abcbd9535522d38342
Prabhat-bhardwaj <prabhatbhardwaj723462@gmail.com>
2025-07-14 21:21:10 +0530

    code for excel

--- src/test/java/ProductDetailsTC.java
+++ src/test/java/ProductDetailsTC.java
@@ -1,26 +1,26 @@
 import java.util.Map;
 import org.testng.annotations.Listeners;
-import listeners.TestListener; 
+import listeners.TestListener;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import Pages.productDetails;
 
 @Listeners(TestListener.class)
 public class ProductDetailsTC extends BaseTest {
+	@Test(dataProvider = "loginData", dataProviderClass = dataproviders.ExcelDataProviders.class)
 
-	@Test
-	public void BaseTest() throws InterruptedException {
-		
-		doLogin("prabhat9759@gmail.com", "Test@123");
+	public void BaseTest(String email, String pwd) throws InterruptedException {
+
+		doLogin(email, pwd);
 		productDetails pd = new productDetails(driver);
 		Thread.sleep(2000);
 		pd.clickProduct();
 //		String allPro = "All Products";
 //		Assert.assertEquals(pd.verifyAllProductsAndBrandList(), allPro);
-		
+
 		pd.clickViewProduct();
 		Thread.sleep(2000);
-		
+
 		Map<String, String> actualDetails = pd.verifyProductDetails();
 		Assert.assertEquals(actualDetails.get("Name"), "Blue Top");
 		Assert.assertEquals(actualDetails.get("Category"), "Category: Women > Tops");
@@ -28,8 +28,7 @@ public class ProductDetailsTC extends BaseTest {
 		Assert.assertEquals(actualDetails.get("Availability"), "Availability: In Stock");
 		Assert.assertEquals(actualDetails.get("Condition"), "Condition: New");
 		Assert.assertEquals(actualDetails.get("Brand"), "Brand: Polo");
-		
-//		Assert.assertEquals(pd.verifyProductDetails(), null);
+
 	}
-	
+
 }
